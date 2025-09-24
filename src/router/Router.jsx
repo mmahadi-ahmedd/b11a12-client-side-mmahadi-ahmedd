c4feaf49379ea540ab21ae50c331f23a454dd308
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-09-24 20:54:50 +0600

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,10 @@
+
+import { createBrowserRouter } from "react-router";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-500" >Hello World</div>,
+  },
+]);
